e03a46b5b2f89912fc09ba8a5fe3dd24bf0fabd5
SarthakXO <sarthakbhatt@live.com>
2024-12-24 23:56:41 +0530

    login added

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ApolloProvider client={client}>{children}</ApolloProvider>
+        {/* <ApolloProvider client={client}> */}
+          {children}
+
+        {/* </ApolloProvider> */}
       </body>
     </html>
   );
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginBox from '@/components/LoginBox'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <LoginBox/>
+    </div>
+  )
+}
+
+export default page
